1ad7743dfd14e5eb537486bc49fa77301f20693f
nikita <nikita.yazkov@yandex.ru>
2024-05-02 21:59:51 +0300

    Add project files.

---
+++ Interfaces.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interfaces.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
