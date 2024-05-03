dd3531e8dc930c3c79f624a587ef25e5a69a1667
nikita <nikita.yazkov@yandex.ru>
2024-05-03 21:06:20 +0300

    Задание 1. Мини-калькулятор, подсчитывающий сумму двух чисел.

---
+++ Interfaces.Task1/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Interfaces.Task1
+{
+    /// <summary>
+    /// Калькулятор
+    /// </summary>
+    internal class Calculator : ICalculator
+    {
+        /// <inheritdoc/>        
+        public int Sum(int a, int b)
+        {
+            return checked(a + b);
+        }
+    }
+}
--- Interfaces.Task1/Program.cs
+++ Interfaces.Task1/Program.cs
@@ -6,7 +6,64 @@ namespace Interfaces.Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("\tКонсольный мини-калькулятор, складывающий два числа.");
+            
+            while (true)
+            {
+                Console.WriteLine();
+                Console.Write("Введите первое слагаемое: ");
+                var firstTerm = ReadNumber();
+
+                Console.Write("Введите второе слагаемое: ");
+                var secondTerm = ReadNumber();
+
+                ICalculator calculator = new Calculator();
+
+                try
+                {
+                    var sum = calculator.Sum(firstTerm, secondTerm);
+
+                    Console.WriteLine("Сумма: {0}", sum);
+                }
+                catch (OverflowException ex)
+                {
+                    Console.WriteLine("Ошибка сложения чисел: {0}", ex.Message);
+                }                                
+
+                Console.WriteLine("Для выхода нажмите 'x', для продолжения - любую другую клавишу.");
+                var exit = Console.ReadLine();
+                if (exit != "x")
+                    continue;
+
+                Console.WriteLine("Работа завершена.");
+                break;
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Считать число с консоли
+        /// </summary>
+        /// <returns>Число</returns>
+        private static int ReadNumber()
+        {            
+            while (true)
+            {
+                try
+                {
+                    var number = Convert.ToInt32(Console.ReadLine());
+
+                    return number;
+                }
+                catch (Exception ex) when (ex is FormatException || ex is OverflowException)
+                {
+                    Console.WriteLine("Ошибка чтения данных. {0}", ex.Message);
+                    Console.WriteLine("Повторите попытку.");
+                    Console.Write("Введите число: ");
+                    continue;
+                }
+            }
         }
     }
 }
